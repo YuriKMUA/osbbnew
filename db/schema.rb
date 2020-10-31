0177fa6d8cc42b4e79ed43e5ca9223bfb6631496
Yurikmua <yurikmua@gmail.com>
2020-11-01 00:37:32 +0200

    Change DB

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_27_070622) do
+ActiveRecord::Schema.define(version: 2020_10_31_220855) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2020_10_27_070622) do
     t.string "remember_token"
     t.integer "position_id"
     t.integer "admin"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
